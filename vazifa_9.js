5913aadce766be30e32caf7d4a5070aa2d94c787
Ismoil Roxataliyev <88145335+izlovchiismoil@users.noreply.github.com>
2023-10-16 17:07:37 +0500

    vazifa 9 yangilandi

--- vazifa_9.js
+++ vazifa_9.js
@@ -1,19 +1,20 @@
 // Palindrome
 
-let n = 12321;
-// let n = 85672;
+// let n = 12321;
+let sanagich = 0;
+let n = 85672;
 
 let a = `${n}`.split("");
 
 for (let i = 1, j = 0; i <= a.length; i++, j++) {
     if (a[j] == a[a.length - i]) {
-        // console.log(a[j]);
-        // console.log(a[a.length - i]);
-        console.log(true);
+        sanagich++;
     }
-    else {
-        console.log(false);
-    }
-    // console.log(a[j]);
-    // console.log(a[a.length - i]);
+}
+
+if (sanagich === a.length) {
+    console.log(true);
+}
+else {
+    console.log(false);
 }
