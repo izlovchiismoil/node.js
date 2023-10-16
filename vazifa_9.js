e4d451583740b4dce4ca0d47a9358af5b1fb3441
Ismoil Roxataliyev <88145335+izlovchiismoil@users.noreply.github.com>
2023-10-16 09:02:51 +0500

    for siklini yozdim

--- vazifa_9.js
+++ vazifa_9.js
@@ -2,3 +2,15 @@
 
 let n = 12321;
 
+let a = `${n}`.split("");
+
+for (let i = 0; i < a.length; i++) {
+    if (a[i] == a[a.length - i]) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+
+// console.log(a);
